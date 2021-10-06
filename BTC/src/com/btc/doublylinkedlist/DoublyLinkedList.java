d80d3c8ff3d9fd02172a67b4608efb843db6a7af
LENOVO <LENOVO@LENOVO-PC>
2021-10-06 12:40:50 +0530

    Updated

--- BTC/src/com/btc/doublylinkedlist/DoublyLinkedList.java
+++ BTC/src/com/btc/doublylinkedlist/DoublyLinkedList.java
@@ -192,21 +192,7 @@ public class DoublyLinkedList
 		System.out.println();
 	}
 	
-	public static void test(int a[])
-	{
-		for(int i=0; i<=a.length-1; i++)
-		{
-			int j = Math.abs(a[i]);
-			if(a[j]>=0)
-			{
-				a[j]=-a[j];
-			}
-			else
-			{
-				System.out.print(j+" ");
-			}
-		}
-	}
+	
 	
 	public void ReverseList()
 	{
